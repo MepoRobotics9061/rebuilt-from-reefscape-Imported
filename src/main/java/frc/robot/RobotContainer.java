e9ade3990fe6aa9d13561353c63fa7e6ec0231f4
noahthedab <smithn@mepoedu.org>
2026-03-19 14:36:06 -0500

    Formatting and Button Changes. *Check Lines 123-126 (Comment)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,15 +4,11 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import frc.robot.Constants;
@@ -29,17 +25,7 @@ import frc.robot.subsystems.Swerve;
 import frc.robot.subsystems.RobotIntakePivot;
 import frc.robot.subsystems.RobotClimber;
 import frc.robot.subsystems.RobotCamera;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-/**
- * This class is where the bulk of the robot should be declared. Since
- * Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in
- * the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of
- * the robot (including
- * subsystems, commands, and button mappings) should be declared here.
- */
 public class RobotContainer {
   /* Controllers */
   private final CommandJoystick driver = new CommandJoystick(0);
@@ -60,16 +46,17 @@ public class RobotContainer {
   private final Swerve s_Swerve = new Swerve(m_robotCamera); 
   
   /* Commands */
-  
   private final GameCommands m_gameCommands;
+
   private final Autos m_autos;
+
   private final SendableChooser<Command> m_autoChooser;
 
+  /* Variables */
   String climbMode = "Off (press twice)";
 
   Boolean isAuto = false;
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
 
     m_gameCommands = new GameCommands(
@@ -96,85 +83,82 @@ public class RobotContainer {
       () -> true,
       () -> 1.0));
 
-    // Configure the button bindings
     configureButtonBindings();
     configureAutos();
 
     SmartDashboard.putString("Climb Mode", climbMode);
   }
 
-  /**
-   * Use this method to define your button->command mappings. Buttons can be
-   * created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
-   * it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
   private void configureButtonBindings() {
+
+    /*
+      XBox:
+        5 / LB = Intake Fuel
+        7 / Back = Push Fuel Out of Intake
+        6 / RB = Fire Fuel
+        8 / Start = Push Fuel Out of Launch Chain
+        4 / Y = Store Intake Mechanism
+        1 / A = Move Intake Mechanism to a Usable Position
+        POV [D-Pad] Up/Down = Move Climber
+        9 / Left Stick = Toggle Climb Mode
+
+      Joystick:
+
+    */
 
     /* Driver Buttons */
 
 
 
-    /*  XBox:
-          5, 6 [Bumpers] = Intake
-          Trigger = Fire
-          1, 2, 3, 4 [A, B, X, Y] = (Coral)/(Algae) Pivots
-          9 [Left Stick]= Toggle Pivots
-          POV [D-Pad] = (Elevator)/(Half Elevator)
-          10 [Right Stick] = Toggle Elevator
-          7, 8 [Select and Start]= Lights
+    /* Operator Buttons */
 
-        Joystick:
-          7 = Center A Tag
-          9 = Reef A Tag
-          11 = Processor A Tag
-        */
+    operator.button(Constants.Controller.LB).whileTrue(
+      m_robotIntake.intake(.36)
+    );
 
-      /* Operator Buttons */
+    operator.button(Constants.Controller.Back).whileTrue(
+      m_robotIntake.launch(.1)
+    );
 
-      operator.button(Constants.Controller.X).whileTrue(
-        m_robotIntake.intake(.36)
-      );
+    /*
+    The two m_robotLaunchChain commands need check to make sure the buttons are right.
+    Start should slowly push the fuel backwards while RB should be the primary fire.
+    */
 
-      operator.button(Constants.Controller.B).whileTrue(
-        m_robotIntake.launch(.1)
-      );
+    operator.button(Constants.Controller.RB).whileTrue(
+      m_robotLaunchChain.indvSpeedCommand(1, 1)
+    );
+    
+    operator.button(Constants.Controller.Start).whileTrue(
+      m_robotLaunchChain.launch(-.1)
+    );
 
-      operator.button(Constants.Controller.LB).whileTrue(
-        m_robotLaunchChain.indvSpeedCommand(-1, -1)
-      );
+    operator.button(Constants.Controller.Y).whileTrue(
+      m_robotIntakePivot.manualPivotMove(6.8)
+    );
 
-      operator.button(Constants.Controller.RB).whileTrue(
-        m_robotLaunchChain.indvSpeedCommand(1, 1)
-      );
+    operator.button(Constants.Controller.A).whileTrue(
+      m_robotIntakePivot.manualPivotMove(0)
+    );
 
-      operator.button(Constants.Controller.Y).whileTrue(
-        m_robotIntakePivot.manualPivotMove(6.8)
-      );
+    operator.povDown().and(() -> climbMode == "On").whileTrue(
+      m_robotClimber.ClimberMove(-.1)
+    );
 
-      operator.button(Constants.Controller.A).whileTrue(
-        m_robotIntakePivot.manualPivotMove(0)
-      );
+    operator.povUp().and(() -> climbMode == "On").whileTrue(
+      m_robotClimber.ClimberMove(.1)
+    );
 
-      operator.povDown().and(() -> climbMode == "On").whileTrue(
-        m_robotClimber.ClimberMove(-.1)
-      );
-
-      operator.povUp().and(() -> climbMode == "On").whileTrue(
-        m_robotClimber.ClimberMove(.1)
-      );
-
-      operator.button(Constants.Controller.LeftStick).onTrue(
-        new InstantCommand( () -> {if(climbMode == "Off"){
+    operator.button(Constants.Controller.LeftStick).onTrue(
+      new InstantCommand( () -> {
+        if(climbMode == "Off"){
           climbMode = "On";
         }else{
           climbMode = "Off";
         }
-          SmartDashboard.putString("Climb Mode", climbMode);
-      })
-      );
+        SmartDashboard.putString("Climb Mode", climbMode);
+    })
+    );
   }
 
   private void configureAutos() {
@@ -188,11 +172,6 @@ public class RobotContainer {
     SmartDashboard.putData("AutoCommand", m_autoChooser);
   }
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
   public Command getAutonomousCommand() {
     return m_autoChooser.getSelected();
   }
